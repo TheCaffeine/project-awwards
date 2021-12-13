3e591d42cb056a75cf69dd154795c13fd9f8c668
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2021-12-13 20:49:33 +0300

    registration form for users to register to the app

--- myawwards/views.py
+++ myawwards/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .forms import SignupForm
+
 
 def index(request):
     return render(request, 'index.html')
 
 
 def signup(request):
-    return render(request, 'registration/signup.html')
+    form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form })
